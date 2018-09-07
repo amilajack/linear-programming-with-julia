0f942048cc02162bb778a0be6890f1ee763c5a60
Amila Welihinda <amilajack@gmail.com>
2018-09-06 19:32:30 -0700

    Use testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,12 @@
 #!/usr/bin/env julia
 
 using LP
-using Base.Test
+using Test
  
-tic()
-println("Test 1")
-@test LP.result[1] ≈ 0.459 atol = 0.01
-@test LP.result[2] ≈ 0 atol = 0.01
-@test LP.result[3] ≈ 0 atol = 0.01
-@test LP.result[4] ≈ 0.5467 atol = 0.01
-toc()
+@testset "Test" begin
+    println("Test 1")
+    @test LP.result[1] ≈ 0.459 atol = 0.01
+    @test LP.result[2] ≈ 0 atol = 0.01
+    @test LP.result[3] ≈ 0 atol = 0.01
+    @test LP.result[4] ≈ 0.5467 atol = 0.01
+end
